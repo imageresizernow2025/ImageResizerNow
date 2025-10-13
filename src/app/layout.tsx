6e89b4bdd42f3b21f2cf7f7c433a52de6bcbe824
imageresizernow2025 <imageresizernow2025@gmail.com>
2025-10-13 22:46:30 +0300

    Force Vercel cache refresh - update version and add comment to break cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
+        {/* Fixed favicon - no more SVG syntax errors */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
